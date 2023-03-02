da8114a7552cf6734a73b0b02176ede90ec46e3b
Muhammad Khubaib <muhammadkhubaib492@gmail.com>
2023-03-02 11:14:49 +0500

    i have added form input and some js

---
+++ BMI.js
@@ -0,0 +1,6 @@
+function calculateBMI(){
+    let heightInputFeet = document.getElementById("heightInputFeet").value;
+    let heightInputInch = document.querySelector("#heightInputInch").value;
+
+    let weightInputKg = document.getElementById("weightInputKg").value
+}
