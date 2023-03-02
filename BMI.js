dcf965a53e419f3af1ad9fd5a389b380206c35b3
Muhammad Khubaib <muhammadkhubaib492@gmail.com>
2023-03-02 12:00:58 +0500

    i have added BMI codin  js

--- BMI.js
+++ BMI.js
@@ -2,5 +2,18 @@ function calculateBMI(){
     let heightInputFeet = document.getElementById("heightInputFeet").value;
     let heightInputInch = document.querySelector("#heightInputInch").value;
 
-    let weightInputKg = document.getElementById("weightInputKg").value
+    let weightInputKg = document.getElementById("weightInputKg").value;
+
+    heightInputFeet = parseInt(heightInputFeet);
+    heightInputInch = parseInt(heightInputInch);
+
+    heightInputInch += heightInputFeet * 12;
+
+    let heightInputMeter = heightInputInch * 2.54 / 100; 
+
+    let heightInputMeterSq = heightInputMeter**2;
+
+    let bodyMassIndex = weightInputKg/heightInputMeterSq;
+    
+    
 }
